e03ebed45c4b2efd4db7f58ea715c078890cb4b9
8story8 <8story8@naver.com>
2025-09-08 18:45:01 +0900

    modify coin api to exchange api

--- src/main/kotlin/io/zero/finance_info/coin/api/CoinApi.kt
+++
@@ -1,7 +0,0 @@
-package io.zero.finance_info.coin.api
-
-import org.springframework.web.bind.annotation.RestController
-
-@RestController
-class CoinApi {
-}
---
+++ src/main/kotlin/io/zero/finance_info/coin/api/ExchangeApi.kt
@@ -0,0 +1,16 @@
+package io.zero.finance_info.coin.api
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+
+@RestController
+@RequestMapping("/exchanges")
+class ExchangeApi {
+
+    @GetMapping("/{exchange}")
+    fun getUserById(@PathVariable exchange: String): Unit {
+    }
+}
